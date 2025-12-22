d9155ca24075bacc69933b2e1e649d1c25540581
iglazkovi <glazkovii2005@gmail.com>
2025-12-22 19:09:35 +0300

    Rewrite getting data from lidar for fixed type with cylindrical projection

--- TestData/script_get_data.py
+++ TestData/script_get_data.py
@@ -1,64 +1,45 @@
 from controller import Robot
-import math
+import matplotlib.pyplot as plt
 import numpy as np
 
-SAVE_PATH = "lidar_scan.npy"
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+lidar = robot.getDevice('lidar')
+lidar.enable(timestep)
 
-SELF_COLLISION_DIST = 0.0
-ROTATION_SPEED = 200.0
-ROTATION_TIME_SEC = 0.5
-SCAN_STEP = 20
+MAX_RANGE = 5
+SAVE_FILENAME = 'lidar_scan.npy'
+is_saved = False
+save_delay = 2.0
 
+plt.ion()
+fig, ax = plt.subplots()
+ax.set_xlim(-MAX_RANGE, MAX_RANGE)
+ax.set_ylim(-MAX_RANGE, MAX_RANGE)
+ax.set_aspect('equal')
+scat = ax.scatter([], [], s=2, c='red')
 
-def main():
-    robot = Robot()
-    timestep = int(robot.getBasicTimeStep())
+while robot.step(timestep) != -1:
+    ranges = np.array(lidar.getRangeImage())
+    fov = lidar.getFov()
+    width = lidar.getHorizontalResolution()
+    angles = np.linspace(fov / 2, -fov / 2, width)
 
-    lidar = robot.getDevice("lidar")
-    lidar.enablePointCloud()
-    lidar.enable(timestep)
-    max_range = lidar.getMaxRange()
+    mask = (ranges < lidar.getMaxRange()) & (ranges > lidar.getMinRange())
 
-    left_motor = robot.getDevice("left wheel motor")
-    right_motor = robot.getDevice("right wheel motor")
+    r_valid = ranges[mask]
+    a_valid = angles[mask]
 
-    left_motor.setPosition(float("inf"))
-    right_motor.setPosition(float("inf"))
+    x = r_valid * np.cos(a_valid)
+    y = r_valid * np.sin(a_valid)
 
-    left_motor.setVelocity(ROTATION_SPEED)
-    right_motor.setVelocity(-ROTATION_SPEED)
+    data = np.stack([x, y], axis=1)
 
-    all_points = []
+    if data.size > 0:
+        scat.set_offsets(data)
 
-    steps_to_rotate = int((ROTATION_TIME_SEC * 1000) / timestep)
-
-    for step in range(steps_to_rotate):
-        if robot.step(timestep) == -1:
-            break
-
-        if step % SCAN_STEP != 0:
-            continue
-
-        point_cloud = lidar.getPointCloud()
-        if not point_cloud:
-            continue
-
-        for point in point_cloud:
-            distance = math.sqrt(
-                point.x * point.x + point.y * point.y + point.z * point.z
-            )
-            if SELF_COLLISION_DIST < distance <= max_range:
-                all_points.append([point.x, point.y])
-
-    left_motor.setVelocity(0.0)
-    right_motor.setVelocity(0.0)
-
-    points = np.array(all_points, dtype=float)
-    np.save(SAVE_PATH, points)
-
-    while robot.step(timestep) != -1:
-        pass
-
-
-if __name__ == "__main__":
-    main()
+    if not is_saved and robot.getTime() > save_delay:
+        np.save(SAVE_FILENAME, data)
+        print(f"Points saved: {len(data)}")
+        is_saved = True
+    plt.pause(0.01)
